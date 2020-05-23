938e481826dfddde9a7808577f5e959e2b3351da
Abdella <abdella.shukur2@gmail.com>
2020-05-23 10:21:41 -0400

    added form tags on register and login ejs

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
 passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
+ clientID: process.env.GOOGLE_CLIENT_ID,
    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: 'http://localhost:3000/auth/google/callback'
  },
