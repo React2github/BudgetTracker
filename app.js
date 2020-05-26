d217e7c0d579decf4a7f442e293bfec3a0272a1a
Abdella <abdella.shukur2@gmail.com>
2020-05-26 11:14:38 -0400

    google auth

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ passport.use(new GoogleStrategy({
  clientID: process.env.GOOGLE_CLIENT_ID,
    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     // callbackURL: 'http://localhost:3000/auth/google/callback',
-    callbackURL: 'http://herokupath' + "/auth/google/callback",
+    callbackURL: "/auth/google/callback",
     proxy: true 
  },
  function(accessToken, refreshToken, profile, done) {
