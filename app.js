ee40c00388e6ed495be1ba7659ea864d24c4b5d5
Abdella <abdella.shukur2@gmail.com>
2020-05-26 11:09:35 -0400

    update

--- app.js
+++ app.js
@@ -63,13 +63,12 @@ passport.use(
 
 // Begin Google Strategy 
 
-absoluteURI = http://herokupath
 
 passport.use(new GoogleStrategy({
  clientID: process.env.GOOGLE_CLIENT_ID,
    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://localhost:3000/auth/google/callback',
-    callbackURL: absoluteURI + "/auth/google/callback",
+    // callbackURL: 'http://localhost:3000/auth/google/callback',
+    callbackURL: 'http://herokupath' + "/auth/google/callback",
     proxy: true 
  },
  function(accessToken, refreshToken, profile, done) {
